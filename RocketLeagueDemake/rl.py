10c49fa0d8eead4244ad9afb42deff8ed021d8f4
Jason Knoll <jasonknoll98@aol.com>
2024-10-01 19:25:41 -0400

    Added blue car

---
+++ RocketLeagueDemake/rl.py
@@ -0,0 +1,45 @@
+import pyxel
+
+"""
+ For my very first "demake", I'm trying to 
+ target Rocket League, mainly sideswipe and it's
+ 2D style of play.
+
+ I'd love to build this on both Pico and here in
+ Pyxel. We'll see how I do!
+"""
+
+SCREEN_HEIGHT = 168
+SCREEN_WIDTH = 256
+
+class App:
+    def __init__(self, width, height):
+        pyxel.init(width, height, quit_key=pyxel.KEY_ESCAPE, title="Retro Car Soccer")
+        pyxel.images[0].load(0, 0,'assets/blue_rl_car.png', incl_colors=True)
+        pyxel.run(self.update, self.draw)
+
+    # Check for player input
+    # Check for collisions
+    # Update x, y values
+    def update(self):
+        pass
+
+    # render everything
+    def draw(self):
+        pyxel.cls(11) # draw some tint to the background while clearing the screen
+
+        self.draw_cars()
+
+    # Draw game objects
+    def draw_walls():
+        pass
+
+    def draw_ball():
+        pass
+
+    def draw_cars(self):
+        # Draw the first car to the screen 
+        pyxel.blt(0, 0, 0, 0, 0, 32, 32, 0)
+
+# Run this SOB
+App(SCREEN_WIDTH, SCREEN_HEIGHT)
