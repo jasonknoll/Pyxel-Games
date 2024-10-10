07860c44ec9dd7d42aba653da8e08e4bea187cf3
Jason Knoll <jasonknoll98@aol.com>
2024-10-10 18:41:04 -0400

    Adjust spritesheet x, y. Added ball class.

--- RocketLeagueDemake/rl.py
+++ RocketLeagueDemake/rl.py
@@ -26,26 +26,38 @@ class Car:
     def __init__(self, color=None):
         self.x = 0
         self.y = 0
-        self.color = color
+        self.color = color # might not need this
 
     def draw(self, sheet_x, sheet_y):
         pyxel.blt(self.x, self.y, 0, sheet_x, sheet_y, 32, 32, colkey=BACKGROUND_COLOR)
 
     def move(self, dir=None):
         # Forward and backward movement for blue
-        if (dir=="f" and self.color == 0):
+        if (dir == "f" and self.color == 0):
             self.x += MOVE_AMOUNT
         elif (dir == "b" and self.color == 0):
             self.x -= MOVE_AMOUNT
 
-        if (dir=='f' and self.color == 1):
+        if (dir == 'f' and self.color == 1):
             self.x -= MOVE_AMOUNT
         elif(dir == 'b' and self.color == 1):
             self.x += MOVE_AMOUNT
 
+# TODO need to add gravity to this fool
+class Ball:
+    def __init__(self):
+        self.x = 0
+        self.y  = 0
+        self.r = 0
+        self.color = 7 # grey
+        self.gravity = 0 # idk how I want to handle the physics
+
+    def draw(self):
+        pass
 
 
 # Maybe add these to App class as member vars
+# NOTE last time I tried, Pyxel wouldn't read the car vars
 
 blue_car = Car(0)
 orange_car = Car(1)
@@ -62,8 +74,8 @@ class App:
         pyxel.init(width, height, quit_key=pyxel.KEY_ESCAPE, title="Retro Car Soccer")
 
         # Add sprites to virutal spritesheet
-        pyxel.images[0].load(0, 0,'assets/blue_rl_car.png', incl_colors=False)
-        pyxel.images[0].load(32, 32, 'assets/orange_rl_car.png', incl_colors=False)
+        pyxel.images[0].load(0, 0,'assets/blue_rl_car.png')
+        pyxel.images[0].load(32, 0, 'assets/orange_rl_car.png')
 
         pyxel.run(self.update, self.draw)
         
@@ -90,20 +102,20 @@ class App:
 
         self.draw_cars()
 
-        pyxel.circ(128, 84, 8, 7) # ball
+        self.draw_ball()
 
     # Draw game objects
     def draw_walls():
         pass
 
-    def draw_ball():
-        pass
+    def draw_ball(self):
+        pyxel.circ(128, 84, 8, 7) # ball
 
     def draw_cars(self):
         # Draw the first car to the screen 
         blue_car.draw(0, 0) # Blue Car
 
-        orange_car.draw(32, 32) # Orange car
+        orange_car.draw(32, 0) # Orange car
 
 # Run this SOB
 App(SCREEN_WIDTH, SCREEN_HEIGHT)
